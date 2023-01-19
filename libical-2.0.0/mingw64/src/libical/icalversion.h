01ad605b522ae68aef4d7f90f074a21fb12ddc03
asctime <you@example.com>
2023-01-19 23:54:37 +1100

    cleanup

--- libical-2.0.0/mingw64/src/libical/icalversion.h
+++
@@ -1,7 +0,0 @@
-#ifndef ICAL_VERSION_H
-#define ICAL_VERSION_H
-
-#define ICAL_PACKAGE "libical"
-#define ICAL_VERSION "2.0"
-
-#endif
--- libical-2.0.0/mingw64/src/libicalss/CMakeFiles/icalss-header.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/icalss-header"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/icalss-header.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
